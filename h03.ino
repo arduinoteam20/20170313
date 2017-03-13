1d5d557b42a49ef4cc912063a9c4711a5ccb73a4
arduinoteam20 <lancer856618@gmail.com>
2017-03-13 22:40:40 +0800

    20170313-1040

---
+++ h03.ino
@@ -0,0 +1,24 @@
+float sensorValue;
+float sensorVoltage;
+void setup() 
+{
+ Serial.begin(9600);
+}
+
+void loop() {
+  float Vin;
+  float R1;
+  int i;
+
+ for (i=0;i<=5;i++)
+ {
+ sensorValue = 0.6* sensorValue + 0.4* analogRead(A1);
+ }
+ sensorVoltage = sensorValue * 5 / 1024;
+ R1 = sensorVoltage * (1082000) / 5;
+ Serial.println(sensorValue);
+ Serial.println(sensorVoltage);
+ Serial.println (R1);
+ delay(500);
+ 
+}
